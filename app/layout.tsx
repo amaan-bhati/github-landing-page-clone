560f2d09d37f6894eca48a1b1017a2e6ce78b305
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-07 11:22:09 +0530

    add meta description

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'GitHub Clone By Amaan Bhati',
-  description: 'GitHub landing page clone',
+  description: 'GitHub landing page, meticulously crafted with Next.js, Tailwind CSS, and TypeScript. It boasts pixel-perfect visual fidelity, replicating the originals text animations, fonts, colors, and even animations. Built on top of Next.js, Tailwind CSS, and TypeScript.',
 }
 
 export default function RootLayout({
