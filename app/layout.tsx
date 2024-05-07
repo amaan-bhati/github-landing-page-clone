a0088d57bb735c887d3222da779e99b031d81d88
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-07 11:18:37 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import Navbar from './components/Navbar/Navbar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'GitHub Clone By Amaan Bhati',
+  description: 'GitHub landing page clone',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {/* <Navbar/> */}
+        {children}
+        </body>
+    </html>
+  )
+}
---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    images: {
+      domains: ['GitHub.GitHubassets.com'],
+    },
+  };
+  
